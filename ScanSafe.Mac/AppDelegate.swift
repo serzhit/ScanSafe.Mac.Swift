966ef5e3544af8dd8184f00690a3b32c53d7964a
Sergey Gitinsky <sergey@gitinsky.com>
2017-05-03 14:21:02 +0300

    Initial Commit

---
+++ ScanSafe.Mac/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  ScanSafe.Mac
+//
+//  Created by Сергей Житинский on 03.05.17.
+//  Copyright © 2017 Git in Sky. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ ScanSafe.Mac/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ScanSafe.Mac
+//
+//  Created by Сергей Житинский on 03.05.17.
+//  Copyright © 2017 Git in Sky. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
