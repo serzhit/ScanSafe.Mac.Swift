d91d07692031ad5cf679cb2896ddd6fd2c066248
Sergey Gitinsky <sergey@gitinsky.com>
2017-06-11 23:49:23 +0300

    New xcodeproj file

---
+++ Package.swift
@@ -0,0 +1,10 @@
+// swift-tools-version:3.1
+
+import PackageDescription
+
+let package = Package(
+    name: "ScanSafe.Mac.Swift",
+    dependencies: [
+       .Package(url: "https://github.com/krzyzanowskim/CryptoSwift.git", majorVersion: 0)
+        ]
+)
