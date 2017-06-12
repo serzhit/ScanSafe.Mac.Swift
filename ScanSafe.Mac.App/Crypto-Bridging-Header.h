da88771dc4e9283072388ffec2dfa2047673bb50
Sergey Gitinsky <sergey@gitinsky.com>
2017-06-12 17:46:21 +0300

    Building is ok.

---
+++ ScanSafe.Mac.App/Crypto-Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  Crypto-Bridging-Header.h
+//  ScanSafe.Mac
+//
+//  Created by Gao on 5/23/17.
+//  Copyright © 2017 Git in Sky. All rights reserved.
+//
+
+#ifndef Crypto_Bridging_Header_h
+#define Crypto_Bridging_Header_h
+
+#import <CommonCrypto/CommonCrypto.h>
+#endif /* Crypto_Bridging_Header_h */
